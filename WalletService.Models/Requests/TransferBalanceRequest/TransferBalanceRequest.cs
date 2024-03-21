50d20b3ea398810324338c53537169b719075675
andresTs <andres91411@gmail.com>
2024-03-21 14:32:34 -0600

    the balance transfer is corrected, since the structure of the expected object did not match.

--- WalletService.Models/Requests/TransferBalanceRequest/TransferBalanceRequest.cs
+++ WalletService.Models/Requests/TransferBalanceRequest/TransferBalanceRequest.cs
@@ -1,11 +1,21 @@
-﻿namespace WalletService.Models.Requests.TransferBalanceRequest;
+﻿using System.Text.Json.Serialization;
+
+namespace WalletService.Models.Requests.TransferBalanceRequest;
 
 public class TransferBalanceRequest
 {
+    [JsonPropertyName("fromAffiliateId")]
     public int FromAffiliateId { get; set; }
-    public string FromUserName { get; set; }
+    
+    [JsonPropertyName("fromUserName")]
+    public string FromUserName { get; set; } = string.Empty;
+    
+    [JsonPropertyName("toUserName")]
     public string ToUserName { get; set; } = string.Empty;
+    
+    [JsonPropertyName("amount")]
     public decimal? Amount { get; set; }
 
+    [JsonPropertyName("securityCode")]
     public string SecurityCode { get; set; } = string.Empty;
 }
